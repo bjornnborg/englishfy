cc5c857d2c3633ed76e5ed33f83f8c8746c46cb8
Bjornn Borg <bjornnborg@gmail.com>
2020-05-04 02:14:54 -0300

    Add challenge picker

---
+++ lib/simple_challenge_picker.rb
@@ -0,0 +1,28 @@
+class SimpleChallengePicker
+
+  def initialize(language_from_to, answers_from_to, random_service)
+    @random_service = random_service
+    @language_from_to = language_from_to
+
+    @lang_from = language_from_to.first
+    @lang_to = language_from_to.last
+
+    @answers_from = answers_from_to.first
+    @answers_to = answers_from_to.last
+  end
+
+  def pick(quantity=1)
+    chosen_language = @random_service.random_language
+    lookup_map = chosen_language == @lang_to ? @answers_to : @answers_from
+
+    terms = lookup_map.keys.sample(quantity)
+    challenge = {
+      language: chosen_language,
+      terms: terms,
+      question: "How do you say #{terms.map(&:upcase).join(',')} in #{chosen_language}?" # refator to support from ES to FR in future
+    }
+
+    challenge
+  end
+
+end
---
+++ spec/simple_challenge_picker_spec.rb
@@ -0,0 +1,72 @@
+require './lib/simple_challenge_picker'
+require './lib/default_random_language_service'
+
+describe 'simple_challenge_picker' do
+
+  english = {
+    "i" => ["eu"],
+    "you" => ["você", "vocês"],
+    "he" => ["ele"],
+    "she" => ["ela"],
+    "it" => ["isso"],
+    "they" => ["eles", "elas"]
+  }
+
+  portuguese = {
+    "eu" => ["i"],
+    "você" => ["you"],
+    "vocês" => ["you"],
+    "ele" => ["he"],
+    "ela" => ["she"],
+    "isso" => ["it"],
+    "eles" => ["they"],
+    "elas" => ["they"]
+  }
+
+  languages = [:portuguese, :english]
+
+  
+  challenge_picker = SimpleChallengePicker.new(languages, [portuguese, english], DefaultRandomLanguageService.new(languages.first, languages.last))
+
+  context 'terms' do
+    it 'picks the right amount of terms' do
+      challenge = challenge_picker.pick(3)
+      expect(challenge[:terms].size).to eq 3
+    end
+
+    it 'uses a default value' do
+      challenge = challenge_picker.pick
+      expect(challenge[:terms].size).to eq 1
+    end
+  end
+
+  context 'chosen language' do
+    it 'always choose a valid language' do
+      200.times do
+        challenge = challenge_picker.pick
+        expect(languages.include?(challenge[:language])).to be_truthy
+      end
+    end
+  end
+
+  context 'question' do
+    it 'contains the only one term' do
+      challenge = challenge_picker.pick
+      expect(challenge[:question].include?(challenge[:terms].first.upcase)).to be_truthy
+    end
+
+    it 'contains all terms' do
+      challenge = challenge_picker.pick(3)
+      challenge[:terms].each do |term|
+        expect(challenge[:question].include?(term.upcase)).to be_truthy
+      end
+    end
+
+    it 'joins all terms' do
+      challenge = challenge_picker.pick(3)
+      joined_terms = challenge[:terms].map(&:upcase).join(',')
+      expect(challenge[:question].include?(joined_terms)).to be_truthy
+    end    
+  end  
+
+end
