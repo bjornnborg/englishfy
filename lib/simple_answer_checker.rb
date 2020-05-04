7bc9faadbeb9073fc445cc5cfdfe10b2be4d17fb
Bjornn Borg <bjornnborg@gmail.com>
2020-05-04 01:02:26 -0300

    Create simple anwser checker

---
+++ lib/simple_answer_checker.rb
@@ -0,0 +1,20 @@
+class SimpleAnswerChecker
+
+  def initialize(language_from_to, answers_from_to)
+    @lang_from = language_from_to.first
+    @lang_to = language_from_to.last
+
+    @answers_from = answers_from_to.first
+    @answers_to = answers_from_to.last
+  end
+
+  def check(challenge, answer)
+    lookup_map = challenge[:language] == @lang_to ? @answers_to : @answers_from
+
+    possible_answers = lookup_map[answer]
+    valid = possible_answers && possible_answers.include?(challenge[:term].first.downcase)  #simple checker supports only one term
+
+    valid
+  end
+
+end
---
+++ spec/simple_answer_checker_spec.rb
@@ -0,0 +1,46 @@
+require './lib/simple_answer_checker'
+
+describe 'simple_answer_checker' do
+
+    english_terms = {
+      "i" => ["eu"],
+      "you" => ["você", "vocês"]
+    }
+
+    portuguese_terms = {
+      "eu" => ["i"],
+      "você" => ["you"],
+      "vocês" => ["you"]
+    }
+
+    answer_checker = SimpleAnswerChecker.new([:portugues, :english], [portuguese_terms, english_terms])
+
+  context 'from -> to translations' do
+
+    it 'founds the expected translation' do
+      challenge = {term: ["você"], language: :english}
+      expect(answer_checker.check(challenge, "you")).to be_truthy
+    end
+
+    it 'does not found the expected translation' do
+      challenge = {term: ["você"], language: :english}
+      expect(answer_checker.check(challenge, "she")).to be_falsy
+    end
+
+  end
+
+  context 'to -> from translation' do
+    it 'founds the expected translation' do
+      challenge = {term: ["you"], language: :portugues}
+      expect(answer_checker.check(challenge, "você")).to be_truthy
+    end
+
+    it 'does not found the expected translation' do
+      challenge = {term: ["you"], language: :portugues}
+      expect(answer_checker.check(challenge, "ele")).to be_falsy
+    end    
+
+  end
+
+
+end
