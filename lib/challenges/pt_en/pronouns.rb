b5ac8d72c50a9f8bd76b83e5ff59b2365f7a16c6
Bjornn Borg <bjornnborg@gmail.com>
2020-05-04 03:08:22 -0300

    Create pt-en pronoun challenge

---
+++ lib/challenges/pt_en/pronouns.rb
@@ -0,0 +1,44 @@
+require_relative('../../simple_challenge_picker')
+
+module Challenges
+  module PtEn
+
+    class Pronouns
+      def initialize
+        @english = {
+          "i" => ["eu"],
+          "you" => ["você", "vocês"],
+          "he" => ["ele"],
+          "she" => ["ela"],
+          "it" => ["isso"],
+          "they" => ["eles", "elas"]
+        }
+
+        @portuguese = {
+          "eu" => ["i"],
+          "você" => ["you"],
+          "vocês" => ["you"],
+          "ele" => ["he"],
+          "ela" => ["she"],
+          "isso" => ["it"],
+          "eles" => ["they"],
+          "elas" => ["they"]
+        }
+
+        @languages = [:portuguese, :english]
+      end
+
+      def build
+        SimpleChallengePicker.new(@languages, [@portuguese, @english], DefaultRandomLanguageService.new(@languages.first, @languages.last))
+      end
+
+      def language_values
+        {
+          @languages.first => @portuguese,
+          @languages.last => @english
+        }
+      end
+    end
+    
+  end
+end
